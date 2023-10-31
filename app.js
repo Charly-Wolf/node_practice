982a3d5bc7af7ddb1d1083d754ebda91a0d8ced9
Charly <cardan1990@hotmail.com>
2023-10-31 21:56:20 +0100

    Created two separated servers

--- app.js
+++ app.js
@@ -1,16 +1,19 @@
 import express, { json } from 'express';
-import { moviesRouter } from './routes/movies.js';
+import { createMovieRouter } from './routes/movies.js';
 import { corsMiddleware } from './middelwares/cors.js';
 
-const app = express();
-app.use(json()); // Middleware
-app.disable('x-powered-by');
-app.use(corsMiddleware()); // Middleware
+export const createApp = ({ movieModel }) => {
+  const app = express();
+  app.use(json()); // Middleware
+  app.disable('x-powered-by');
+  app.use(corsMiddleware()); // Middleware
+  
+  app.use('/movies', createMovieRouter({ movieModel }));
+  
+  const PORT = process.env.PORT ?? 1234;
+  
+  app.listen(PORT, () => {
+    console.log(`server listening on port http://localhost:${PORT}`);
+  });
+}
 
-app.use('/movies', moviesRouter);
-
-const PORT = process.env.PORT ?? 1234;
-
-app.listen(PORT, () => {
-  console.log(`server listening on port http://localhost:${PORT}`);
-});
