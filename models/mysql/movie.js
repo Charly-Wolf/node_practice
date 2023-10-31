c90ea71015c96e3319cad83e08606d0c9575b491
Charly <cardan1990@hotmail.com>
2023-10-31 20:21:04 +0100

    Added get movie by genre

--- models/mysql/movie.js
+++ models/mysql/movie.js
@@ -12,6 +12,29 @@ const connection = await mysql.createConnection(config);
 
 export class MovieModel {
   static async getAll({ genre }) {
+    if (genre) {
+      const lowerCaseGenre = genre.toLowerCase();
+
+      // get genre ids from database table using genre names
+      const [genres] = await connection.query(
+        'SELECT id, name FROM genre WHERE LOWER(name) = ?;',
+        [lowerCaseGenre]
+      );
+
+      // no genre found
+      if (genres.length === 0) return [];
+
+      // get id from the first genre result
+      const [{ id }] = genres;
+
+      // get all movies ids from database table
+      const [moviesByGenre] = await connection.query(
+        'SELECT BIN_TO_UUID(m.id) id, title, year, director, duration, poster, rate FROM movie m JOIN movie_genres mg ON m.id = mg.movie_id JOIN genre g ON mg.genre_id = g.id WHERE g.id = ?', [id]
+      );
+
+      return moviesByGenre;
+    }
+
     const [movies] = await connection.query(
       'SELECT BIN_TO_UUID(id) id, title, year, director, duration, poster, rate FROM movie;'
     );
